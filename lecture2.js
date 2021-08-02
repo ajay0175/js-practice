ddbb3f0e607283d240985e81cbd12d553bbddeae
ajay0175 <75410076+ajay0175@users.noreply.github.com>
2021-08-02 18:20:37 +0530

    Add files via upload

---
+++ lecture2.js
@@ -0,0 +1,24 @@
+var num = 10;
+var dec = 10.3;
+var bool  = true;
+var str = "string";
+var object ={};
+
+console.log(num , dec ,bool , str);
+console.log(typeof num , typeof dec , typeof bool ,typeof str, typeof object);
+
+
+// objects
+
+var x = {};
+var y = {"name": "Ajaykumar"};
+var z = {name: "Ajaykumar"};
+x.name = "Ajaykumar";
+
+console.log(x, y, z);
+
+///////
+
+console.log(1+"2");
+console.log(1-"2");
+console.log(1- "Ajaykumar");
